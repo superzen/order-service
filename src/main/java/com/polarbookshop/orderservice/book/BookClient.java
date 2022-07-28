e08a9978870751e52794fa03fbdf04f912c3192f
Jesus Ciriano <jciriano@nalandaglobal.com>
2022-07-28 09:37:14 +0200

    8.4.2 Retries

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -3,6 +3,7 @@ package com.polarbookshop.orderservice.book;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -22,6 +23,7 @@ public class BookClient {
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
                 .bodyToMono(Book.class)
-                .timeout(Duration.ofSeconds(3), Mono.empty());
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)));
     }
 }
